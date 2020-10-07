a930424b5047312367b3c2a8144e1e33ead3b03b
Kushal Chowdhury <kushalbabay@gmail.com>
2020-10-08 00:19:15 +0530

    Add files via upload

---
+++ precache-manifest.3e9ca0a5f8e2de929af9f78ff903b890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532aabcb7a7ae2bd1d52cdab9b5f70a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7ce09b5273407d5465a",
+    "url": "/static/css/main.100aa4f9.chunk.css"
+  },
+  {
+    "revision": "557645e3b01d2d9bd613",
+    "url": "/static/js/2.afdc3f3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.afdc3f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ce09b5273407d5465a",
+    "url": "/static/js/main.fc9c160a.chunk.js"
+  },
+  {
+    "revision": "8cab146f387c6fa6faf4",
+    "url": "/static/js/runtime-main.03d328f6.js"
+  }
+]);
